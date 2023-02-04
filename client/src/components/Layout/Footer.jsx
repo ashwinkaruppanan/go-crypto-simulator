de4ba20875fcddef1aecd5933027434df3400cb7
ashwinkaruppanan <ashwinak148@gmail.com>
2023-02-04 19:21:17 +0530

    cplay

--- client/src/components/Layout/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return <>
-    <div className="footer">
-        <p>copyright &#169;	{new Date().getFullYear()}</p>
-    </div>
-    </>
-}
-
-export default Footer;
---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import {createRoot } from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from "./components/App";
+import './styles.css';
+
+
+const rootElement  = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(<App />)
